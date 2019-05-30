93c7a60ac76b66d52fbc21d0b60de80ef52bf351
Steven Massaro <steven.massaro.web@gmail.com>
2019-05-30 16:18:18 -0400

    extract profile to separate endpoint

--- src/main/java/rcwd/endpoint/ProfileEndpoint.java
+++ src/main/java/rcwd/endpoint/ProfileEndpoint.java
@@ -5,26 +5,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import rcwd.mapper.CommandMapper;
 import rcwd.mapper.ProfileMapper;
-import rcwd.model.Command;
 import rcwd.model.Profile;
 import rcwd.service.ExecutionService;
 
-@RestController()
-@RequestMapping("/execute")
-public class ExecuteEndpoint {
+import java.util.List;
+
+@RestController
+@RequestMapping("/profile")
+public class ProfileEndpoint {
 
     @Autowired
-    private CommandMapper commandMapper;
+    private ProfileMapper profileMapper;
 
     @Autowired
     private ExecutionService executionService;
 
-    @GetMapping("/command/{commandId}")
-    public String executeByCommandId(@PathVariable long commandId){
-        Command command = commandMapper.get(commandId);
-        executionService.execute(command);
-        return Long.toString(command.getId());
+    @GetMapping
+    public List<Profile> list(){
+        return profileMapper.list();
+    }
+
+    @GetMapping("/{profileId}/execute")
+    public String executeByProfileId(@PathVariable long profileId) {
+        Profile profile = profileMapper.get(profileId);
+        executionService.execute(profile.getCommands());
+        return Long.toString(profile.getId());
     }
 }
