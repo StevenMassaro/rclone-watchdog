4a86bab67a7500d56d313aaa9c3dfa1ddec0856b
Steven Massaro <steven.massaro.web@gmail.com>
2022-01-01 09:56:55 -0600

    support hiding command from UI

--- updatetables.sql
+++ updatetables.sql
@@ -1,2 +1,5 @@
 alter table rclonewatchdog.command add column filter int null;
 alter table rclonewatchdog.command drop column filters cascade;
+
+alter table rclonewatchdog.command add column hidden boolean;
+update rclonewatchdog.command set hidden = false;
