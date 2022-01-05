250067e100904b742717d11892990d5b8ea2f8ac
Steven Massaro <steven.massaro.web@gmail.com>
2022-01-05 17:46:20 -0600

    expand integration test for command mapper

--- src/test/java/rcwd/CommandMapperIT.java
+++ src/test/java/rcwd/CommandMapperIT.java
@@ -7,11 +7,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import rcwd.mapper.CommandMapper;
+import rcwd.mapper.StatusMapper;
 import rcwd.model.Command;
+import rcwd.model.StatusEnum;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,10 +25,28 @@ public class CommandMapperIT {
     @Autowired
     private CommandMapper commandMapper;
 
+    @Autowired
+    private StatusMapper statusMapper;
+
+    @Autowired
+    private DataSource dataSource;
+
     @Test
-    public void list() {
+    public void list() throws SQLException {
         List<Command> list = commandMapper.list();
         assertTrue(list.isEmpty());
+
+        dataSource.getConnection().prepareCall("INSERT INTO rclonewatchdog.\"source\" (id, \"name\", directory) VALUES(1, 'unRAID Backups', '/data/disks/Temporary/Backups');").execute();
+        dataSource.getConnection().prepareCall("INSERT INTO rclonewatchdog.destination (id, \"name\", remote, directory) VALUES(1, 'Google Drive 2TB Encrypted: AutomatedUnRaid', 'GoogleDrive-Limited-Crypt', 'AutomatedUnRaid');").execute();
+        dataSource.getConnection().prepareCall("INSERT INTO rclonewatchdog.\"filter\" (id, \"filter\") VALUES(1, '--exclude=Windows Images/**');").execute();
+        dataSource.getConnection().prepareCall("INSERT INTO rclonewatchdog.command (name, command, \"source\", destination, \"filter\", hidden) VALUES('test', 'sync', 1, 1, 1, false);").execute();
+
+        statusMapper.insert(1, StatusEnum.DRY_RUN_EXECUTION_START, null);
+
+        list = commandMapper.list();
+        assertEquals(1, list.size());
+        assertNotNull(list.get(0).getStatus());
+        assertNotNull(list.get(0).getStatusChangeDate());
     }
 
 }
