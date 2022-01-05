029c4a3ed0d6d9a156dd0bccd807e157d9d90ac1
Steven Massaro <steven.massaro.web@gmail.com>
2022-01-05 17:49:54 -0600

    don't run startup validator during tests

--- src/main/java/rcwd/StartupValidator.java
+++ src/main/java/rcwd/StartupValidator.java
@@ -2,26 +2,32 @@ package rcwd;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import rcwd.properties.RcwdProperties;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
+import java.util.Arrays;
 
 @Component
 @Log4j2
 public class StartupValidator {
     private final RcwdProperties properties;
+    private final Environment environment;
 
-    public StartupValidator(RcwdProperties properties) {
+    public StartupValidator(RcwdProperties properties, Environment environment) {
         this.properties = properties;
+        this.environment = environment;
     }
 
     @PostConstruct
     public void validateRclonePath() throws Exception {
-        File rcloneExecutable = new File(properties.getRcloneBasePath());
-        if (!rcloneExecutable.exists()) {
-            throw new Exception("rclone executable at location " + properties.getRcloneBasePath() + " does not exist");
+        if (!Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+            File rcloneExecutable = new File(properties.getRcloneBasePath());
+            if (!rcloneExecutable.exists()) {
+                throw new Exception("rclone executable at location " + properties.getRcloneBasePath() + " does not exist");
+            }
         }
     }
 }
